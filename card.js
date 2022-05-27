e0238d8c948dc97edf6e161816f32e16c32f7bd9
grassfinn <theharper@live.com>
2022-05-26 23:52:35 -0400

    Major Refacotoring of the classes

--- card.js
+++ card.js
@@ -1,15 +1,21 @@
 class Card {
+  // constructor
+  constructor(data) {
     // properties
-    title;
-
-    // image;
-
-    link;
-    // constructor
-    constructor(title, link) {
-        this.title = title
-        // this.image = image
-        this.link = link
-    }
-    // methods
+    Object.assign(this, data);
+  }
+  // methods
+  getCardHtml() {
+    const { title, deployment, thumbnail, repo } = this;
+    return `<article class='portfolio-item'>
+    <h3> ${title} </h3>
+    <img class= 'portfolio-img 'src='${thumbnail}'>
+      <div>
+      <a class='portfolio-link' href='${deployment}' target='_blank'> Live site </a>
+      <a class='portfolio-link' href='${repo}' target='_blank'> GitHub </a>
+      </div>
+      </article>`;
+  }
 }
+
+export default Card
---
+++ data.js
@@ -0,0 +1,45 @@
+const cardData = {
+  blackjackGame: {
+    title: 'Blackjack',
+    deployment: 'https://grassfinn.github.io/Blackjack-Game/',
+    thumbnail: './thumbnails/1x/300blackjack.PNG',
+    repo: 'https://github.com/grassfinn/Blackjack-Game',
+  },
+
+  parallax: {
+    title: 'Adventure Time Parallax Site',
+    deployment: 'https://grassfinn.github.io/Adventure-Time-Parallax-Site/',
+    thumbnail: './thumbnails/parallax.PNG',
+    repo: 'https://github.com/grassfinn/Adventure-Time-Parallax-Site',
+  },
+
+  unitConversion: {
+    title: 'Unit Conversion',
+    deployment: 'https://grassfinn.github.io/Unit-Conversion/',
+    thumbnail: './thumbnails/1x/300unit conversion.PNG',
+    repo: 'https://github.com/grassfinn/Unit-Conversion',
+  },
+
+  passwordGenerator: {
+    title: 'Password Generator',
+    deployment: 'https://grassfinn.github.io/Password-Generator/',
+    thumbnail: './thumbnails/1x/300generator.PNG',
+    repo: 'https://github.com/grassfinn/Password-Generator',
+  },
+
+  wordle: {
+    title: 'Custom Input Wordle',
+    deployment: 'https://grassfinn.github.io/Wordle-with-input/',
+    thumbnail: './thumbnails/1x/300wordle.PNG',
+    repo: 'https://github.com/grassfinn/Wordle-with-input',
+  },
+
+  pomodoroTimer: {
+    title: 'Pomodoro Timer',
+    deployment: 'https://grassfinn.github.io/pomodoroTimer/',
+    thumbnail: './thumbnails/1x/300pomodoro.PNG',
+    repo: 'https://github.com/grassfinn/pomodoroTimer',
+  },
+};
+
+export default cardData
