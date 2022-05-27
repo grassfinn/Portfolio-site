fb2d59ce0736fc753b3a18e7ccb52ad9e7d2cd2d
grassfinn <theharper@live.com>
2022-05-27 00:14:28 -0400

    github images test2

--- modules/data.js
+++ modules/data.js
@@ -2,42 +2,42 @@ const cardData = {
   blackjackGame: {
     title: 'Blackjack',
     deployment: 'https://grassfinn.github.io/Blackjack-Game/',
-    thumbnail: './thumbnails/1x/300blackjack.png',
+    thumbnail: 'thumbnails/1x/300blackjack.png',
     repo: 'https://github.com/grassfinn/Blackjack-Game',
   },
 
   parallax: {
     title: 'Adventure Time Parallax Site',
     deployment: 'https://grassfinn.github.io/Adventure-Time-Parallax-Site/',
-    thumbnail: './thumbnails/parallax.PNG',
+    thumbnail: 'thumbnails/parallax.PNG',
     repo: 'https://github.com/grassfinn/Adventure-Time-Parallax-Site',
   },
 
   unitConversion: {
     title: 'Unit Conversion',
     deployment: 'https://grassfinn.github.io/Unit-Conversion/',
-    thumbnail: './thumbnails/1x/300unit conversion.png',
+    thumbnail: 'thumbnails/1x/300unit conversion.png',
     repo: 'https://github.com/grassfinn/Unit-Conversion',
   },
 
   passwordGenerator: {
     title: 'Password Generator',
     deployment: 'https://grassfinn.github.io/Password-Generator/',
-    thumbnail: './thumbnails/1x/300generator.png',
+    thumbnail: 'thumbnails/1x/300generator.png',
     repo: 'https://github.com/grassfinn/Password-Generator',
   },
 
   wordle: {
     title: 'Custom Input Wordle',
     deployment: 'https://grassfinn.github.io/Wordle-with-input/',
-    thumbnail: './thumbnails/1x/300wordle.png',
+    thumbnail: 'thumbnails/1x/300wordle.png',
     repo: 'https://github.com/grassfinn/Wordle-with-input',
   },
 
   pomodoroTimer: {
     title: 'Pomodoro Timer',
     deployment: 'https://grassfinn.github.io/pomodoroTimer/',
-    thumbnail: './thumbnails/1x/300pomodoro.png',
+    thumbnail: 'thumbnails/1x/300pomodoro.png',
     repo: 'https://github.com/grassfinn/pomodoroTimer',
   },
 };
