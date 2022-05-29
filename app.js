252aaae47bd9eb9be298dac13ad56010f2b0f4e5
grassfinn <theharper@live.com>
2022-05-29 17:59:14 -0400

    using array methods to display the cards

--- app.js
+++ app.js
@@ -2,15 +2,6 @@ import cardData from "./modules/data.js"
 import Card from "./modules/card.js"
 
 const portfolioEl = document.querySelector('#portfolio-container')
-// new cards
-const blackjack = new Card(cardData.blackjackGame)
-const parallax = new Card(cardData.parallax)
-const passwordGenerator = new Card(cardData.passwordGenerator)
-const pomodoroTimer = new Card(cardData.pomodoroTimer)
-const unitConversion = new Card(cardData.unitConversion)
-const worlde = new Card(cardData.wordle)
-
-
 
 const darkMode = document.getElementById('dark-mode')
 
@@ -18,12 +9,17 @@ darkMode.addEventListener('click', () => {
   document.querySelector('#portfolio').classList.toggle('dark-mode')
 })
 
+
 function render(){
-    portfolioEl.innerHTML = blackjack.getCardHtml()
-    portfolioEl.innerHTML += parallax.getCardHtml()
-    portfolioEl.innerHTML += passwordGenerator.getCardHtml()
-    portfolioEl.innerHTML += pomodoroTimer.getCardHtml()
-    portfolioEl.innerHTML += unitConversion.getCardHtml()
-    portfolioEl.innerHTML += worlde.getCardHtml()
-  }
+  const portfolioArr = ['blackjackGame', 'parallax', 'passwordGenerator', 'pomodoroTimer', 'unitConversion', 'wordle']
+  portfolioArr.forEach( item =>  {
+    const itemArr = []
+    itemArr.push(new Card(cardData[item]))
+    console.log(itemArr)
+    itemArr.forEach(card => {
+      portfolioEl.innerHTML += card.getCardHtml()
+    })
+  })
+
+} 
 render()
